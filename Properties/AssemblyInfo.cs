bd5438cf97ae1faa511062388dabc6218b1f2f57
Peter O <poccil14@gmail.com>
2014-01-19 05:44:48 -0500

    bump up version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.12.0.0")]
+[assembly: AssemblyVersion("0.13.0.0")]
