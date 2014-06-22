acdf972f251d2a7b8b02c02becfbbf4491ba68ec
Peter O <poccil14@gmail.com>
2014-06-22 16:26:28 -0400

    Starting next version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using System.Reflection;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyVersion("2.0.0-SNAPSHOT")]
