630db96b32158a20b6584feaa7ca7974bf91a185
Peter O <poccil14@gmail.com>
2015-02-18 10:03:23 -0500

    update donation URL

--- PeterO/SortedMap.cs
+++ PeterO/SortedMap.cs
@@ -3,7 +3,7 @@ Written in 2013 by Peter O.
 Any copyright is dedicated to the Public Domain.
 http://creativecommons.org/publicdomain/zero/1.0/
 If you like this, you should donate to Peter O.
-at: http://upokecenter.com/d/
+at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
  */
 using System;
 using System.Collections.Generic;
