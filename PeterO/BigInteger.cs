b5d0688e4a5308fce4d38811e5de026cf056da7d
Peter O <poccil14@gmail.com>
2015-02-18 10:03:23 -0500

    update donation URL

--- PeterO/BigInteger.cs
+++ PeterO/BigInteger.cs
@@ -8,7 +8,7 @@ CryptoPP by Wei Dai.
 Any copyright is dedicated to the Public Domain.
 http://creativecommons.org/publicdomain/zero/1.0/
 If you like this, you should donate to Peter O.
-at: http://upokecenter.com/d/
+at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
  */
 using System;
 
--- PeterO/RedBlackTree.cs
+++ PeterO/RedBlackTree.cs
@@ -2,7 +2,7 @@
 Any copyright is dedicated to the Public Domain.
 http://creativecommons.org/publicdomain/zero/1.0/
 If you like this, you should donate to Peter O.
-at: http://upokecenter.com/d/
+at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
  */
 using System;
 using System.Collections.Generic;
