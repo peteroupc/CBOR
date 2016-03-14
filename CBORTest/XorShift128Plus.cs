4bd72c7200fcdfc01ce4513296c30dc16bf3d506
Peter O <poccil14@gmail.com>
2016-03-14 08:20:58 -0400

    minor edit in new RNG

--- CBORTest/XorShift128Plus.cs
+++ CBORTest/XorShift128Plus.cs
@@ -166,8 +166,8 @@ namespace PeterO {
           long s0 = this.s[1];
            this.s[0] = s0;
            s1 ^= s1 << 23;
-           long t1 = (s1 >> 18) & 0x3fffffffffff;
-           long t0 = (s0 >> 5) & 0x7ffffffffffffff;
+           long t1 = (s1 >> 18) & 0x3fffffffffffL;
+           long t0 = (s0 >> 5) & 0x7ffffffffffffffL;
            this.s[1] = s1 ^ s0 ^ t1 ^ t0;
            return unchecked(this.s[1] + s0);
     }
