3653632635dc1957f0e3255001d5e3cf8f574910
Peter O <poccil14@gmail.com>
2018-10-01 16:06:41 -0400

    start next version; etc.

--- CBORDocs2/Properties/AssemblyInfo.cs
+++ CBORDocs2/Properties/AssemblyInfo.cs
@@ -7,9 +7,7 @@ using System.Reflection;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Peter Occil")]
-[assembly:
-
-  AssemblyCopyright(
+[assembly: AssemblyCopyright(
   "Written by Peter O. in 2013-2015. Any copyright is " +
   "dedicated to the Public Domain. " +
   "<http://creativecommons.org/publicdomain/zero/1.0/>")]
