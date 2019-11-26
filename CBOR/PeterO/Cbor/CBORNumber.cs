3dbd8be9f02b183fbb952614edd9cfe99f4c3136
Peter O <poccil14@gmail.com>
2019-11-25 23:55:30 -0500

    Edit documentation; minor code edits

--- CBOR/PeterO/Cbor/CBORNumber.cs
+++ CBOR/PeterO/Cbor/CBORNumber.cs
@@ -91,7 +91,6 @@ namespace PeterO.Cbor {
 
   /// <summary>Gets a value not documented yet.</summary>
   /// <summary>Gets a value not documented yet.</summary>
-  /// <summary>Gets a value not documented yet.</summary>
   /// <value>A value not documented yet.</value>
     public int Sign {
       get {
